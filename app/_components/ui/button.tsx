b257ad12aa6903c6ca3cada028727d2e7004581f
Joao Pedro <joao.fpsilva98@gmail.com>
2025-05-01 13:52:35 -0300

    feat: adding hover and cursor pointer css

--- app/_components/ui/button.tsx
+++ app/_components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "",
+        default: "hover:bg-secondary/90 cursor-pointer",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
