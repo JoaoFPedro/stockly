94ab045bb722dba285aeb7ec42a1ab27284de956
Joao Pedro <joao.fpsilva98@gmail.com>
2025-05-06 19:12:55 -0300

    refac: moving server actions to a separete folder

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,16 +1,16 @@
 "use server";
 import { DataTable } from "@/app/_components/ui/data-table";
 import { productsColum } from "./_components/table-columns";
-import { db } from "@/app/_lib/prisma";
 import { Button } from "@/app/_components/ui/button";
 import { PlusIcon } from "lucide-react";
+import { getProducts } from "../_data-access/product/get-products";
 
 const ProductsPage = async () => {
-  const prisma = await db.product.findMany({});
-  const plainProducts = prisma.map((products) => ({
-    ...products,
-    price: products.price.toNumber(), // ou toString() se preferir manter precisão
-  }));
+  const products = await getProducts();
+  // const plainProducts = prisma.map((products) => ({
+  //   ...products,
+  //   price: products.price.toNumber(), // ou toString() se preferir manter precisão
+  // }));
 
   return (
     <div className="w-full space-y-8 p-8">
@@ -24,7 +24,7 @@ const ProductsPage = async () => {
           </Button>
         </div>
       </div>
-      <DataTable columns={productsColum} data={prisma} />
+      <DataTable columns={productsColum} data={products} />
     </div>
   );
 };
