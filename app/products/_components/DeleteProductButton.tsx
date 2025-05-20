7418e5a9040808cbc84732343455f255cc7b29fe
Joao Pedro <joao.fpsilva98@gmail.com>
2025-05-20 20:48:07 -0300

    feat: adding delation alert

--- app/products/_components/DeleteProductButton.tsx
+++ app/products/_components/DeleteProductButton.tsx
@@ -1,6 +1,16 @@
 import { deleteProduct } from "@/app/_actions/delete-products";
 import { Button } from "@/app/_components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
 import { Product } from "@/app/generated/prisma/client";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { Trash2Icon } from "lucide-react";
 
 interface DeleteProductButtonProps {
   product: Product;
@@ -13,7 +23,33 @@ const DeleteProductButton = ({ product }: DeleteProductButtonProps) => {
   };
   return (
     <>
-      <Button onClick={handleDeleteButton}>Delete Button</Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="ghost">
+            <Trash2Icon />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>O produto será completamente excluído</DialogTitle>
+          </DialogHeader>
+
+          <DialogFooter>
+            <DialogClose asChild>
+              <div className="space-x-2.5">
+                <Button type="submit">Cancelar</Button>
+                <Button
+                  onClick={handleDeleteButton}
+                  variant="secondary"
+                  className="bg-red-800 text-white"
+                >
+                  Deletar
+                </Button>
+              </div>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
