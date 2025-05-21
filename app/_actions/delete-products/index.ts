54ea825fa26921871ee93a55e523568f00832e81
Joao Pedro <joao.fpsilva98@gmail.com>
2025-05-21 20:48:45 -0300

    feat: creating deleteProductschema

--- app/_actions/delete-products/index.ts
+++ app/_actions/delete-products/index.ts
@@ -1,12 +1,18 @@
 "use server";
 import { db } from "@/app/_lib/prisma";
 import { revalidatePath } from "next/cache";
+import { deleteProductSchema, DeleteProductSchema } from "./schema";
 
-export const deleteProduct = async (id: string) => {
-  await db.product.delete({
-    where: {
-      id: id ?? "",
-    },
-  });
+export const deleteProduct = async ({ id }: DeleteProductSchema) => {
+  deleteProductSchema.parse({ id });
+  try {
+    await db.product.delete({
+      where: {
+        id: id ?? "",
+      },
+    });
+  } catch (error) {
+    console.error("ERROR: at Server action delete product:", error);
+  }
   revalidatePath("/");
 };
