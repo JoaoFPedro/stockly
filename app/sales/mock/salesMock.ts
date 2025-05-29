e369f43dd755ee052f585d34ed926f119bc1e87a
Joao Pedro <joao.fpsilva98@gmail.com>
2025-05-29 19:53:13 -0300

    chore: creating mock

---
+++ app/sales/mock/salesMock.ts
@@ -0,0 +1,34 @@
+export const salesMock = [
+  {
+    id: "4cb949e2-67c6-4e2d-bcd6-9f8f13cdb9c0",
+    name: "Camisa Oakley",
+    quantity: 45,
+    stock: 300,
+    createdAt: "2025-05-02T22:58:46.880Z",
+    updatedAt: "2025-05-02T22:58:27.871Z",
+  },
+  {
+    id: "5a10d5d0-172b-478c-a5ea-698f7d945377",
+    name: "Tenis de bandido",
+    quantity: 200,
+    stock: 3,
+    createdAt: "2025-05-17T16:43:04.292Z",
+    updatedAt: "2025-05-21T23:05:00.240Z",
+  },
+  {
+    id: "9a5a8139-450c-4a20-8725-9fb531679a94",
+    name: "Macumba Sarava",
+    quantity: 500,
+    stock: 1,
+    createdAt: "2025-05-19T22:41:02.125Z",
+    updatedAt: "2025-05-21T23:06:52.519Z",
+  },
+  {
+    id: "59f22dcb-8dec-497f-9c5b-ab1ec35d1622",
+    name: "Gorila Haze",
+    quantity: 140,
+    stock: 1,
+    createdAt: "2025-05-19T22:48:32.139Z",
+    updatedAt: "2025-05-22T23:31:10.301Z",
+  },
+];
