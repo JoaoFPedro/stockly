0d5bf629b0cd481c1c4d8d75dabebb850b5979f2
Joao Pedro <joao.fpsilva98@gmail.com>
2025-05-30 23:41:58 -0300

    feat: adding combobox component

--- app/sales/_components/add-sale-button.tsx
+++ app/sales/_components/add-sale-button.tsx
@@ -1,23 +1,17 @@
 "use client";
 import { Button } from "@/app/_components/ui/button";
-import { PlusIcon } from "lucide-react";
-import { useState } from "react";
-import SaleForm from "./sale-form";
+import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet";
+import UpsertSaleDialog from "./upsert-sale-dialog";
 
 const SaleButton = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
-      <Button
-        className="hover:bg-ghost bg-green-500"
-        onClick={() => setIsOpen(true)}
-      >
-        <PlusIcon />
-        Adicionar Venda
-      </Button>
-
-      <SaleForm isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Sheet>
+        <SheetTrigger asChild className="hover:bg-ghost bg-green-500">
+          <Button>Adicionar venda </Button>
+        </SheetTrigger>
+        <UpsertSaleDialog />
+      </Sheet>
     </>
   );
 };
---
+++ app/sales/_components/combobox-sales.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import * as React from "react";
+import { CheckIcon, ChevronsUpDownIcon } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/app/_components/ui/popover";
+import { Button } from "@/app/_components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/app/_components/ui/command";
+import { cn } from "@/app/_lib/utils";
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
+
+export function ComboboxSales() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? frameworks.find((framework) => framework.value === value)?.label
+            : "Selecione um Produto"}
+          <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." />
+          <CommandList>
+            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandGroup>
+              {frameworks.map((framework) => (
+                <CommandItem
+                  key={framework.value}
+                  value={framework.value}
+                  onSelect={(currentValue: React.SetStateAction<string>) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  <CheckIcon
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0",
+                    )}
+                  />
+                  {framework.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
