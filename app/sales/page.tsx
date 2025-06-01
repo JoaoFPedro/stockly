ad83af9818b9c2e6df7ef4fd6a009ffbf218bb23
Joao Pedro <joao.fpsilva98@gmail.com>
2025-06-01 12:16:16 -0300

    feat: adding products prop at upsertSaleDialog

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -2,21 +2,17 @@ import { Button } from "../_components/ui/button";
 import { DataTable } from "../_components/ui/data-table";
 import { Sheet, SheetTrigger } from "../_components/ui/sheet";
 import { getProducts } from "../_data-access/product/get-products";
-import { ComboboxOption } from "./_components/combobox-sales";
-import { SalesColum } from "./_components/table-columns";
+import { ComboboxOption } from "../_components/ui/combobox";
 import UpsertSaleDialog from "./_components/upsert-sale-dialog";
-import { salesMock } from "./mock/salesMock";
 
 const SalesPage = async () => {
-  const mockTest = salesMock;
   const products = await getProducts();
 
   const productOptions: ComboboxOption[] = products.map((product) => ({
-    value: product.id,
     label: product.name,
+    value: product.id,
   }));
 
-  console.log("PRODUCTS****", products);
   return (
     <div className="w-full space-y-8 p-8">
       <div className="w-full">
@@ -29,14 +25,17 @@ const SalesPage = async () => {
             <SheetTrigger asChild className="bg-green-500">
               <Button className="hover:bg-ghost">Adicionar venda </Button>
             </SheetTrigger>
-            <UpsertSaleDialog products={productOptions} />
+            <UpsertSaleDialog
+              productOptions={productOptions}
+              products={products}
+            />
           </Sheet>
         </div>
       </div>
-      <DataTable
+      {/* <DataTable
         columns={SalesColum}
         data={JSON.parse(JSON.stringify(mockTest))}
-      />
+      /> */}
     </div>
   );
 };
