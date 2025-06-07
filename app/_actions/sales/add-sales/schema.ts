255cc46b13e87410dca6eebb30bc6da5ce4540c7
Joao Pedro <joao.fpsilva98@gmail.com>
2025-06-07 20:09:16 -0300

    .

--- app/_actions/sales/add-sales/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const upsertSalesSchema = z.object({
-  productId: z.string().uuid({ message: "O produto é obrigatório" }),
-  quantity: z.number().int().positive(),
-});
-
-export type UpsertSaleSchema = z.infer<typeof upsertSalesSchema>;
