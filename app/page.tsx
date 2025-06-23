749f65346ca29da662d6b4a20100ac7e0975b3d2
Joao Pedro <joao.fpsilva98@gmail.com>
2025-06-23 19:28:05 -0300

    feat: initialize dashboard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import ProductsPage from "./products/page";
+import Header, {
+  HeaderLeft,
+  HeaderSubTitle,
+  HeaderTitle,
+} from "./_components/header";
 
 export default async function Home() {
   return (
-    <>
-      <ProductsPage />
-    </>
+    <div className="w-full space-y-8 p-8">
+      <Header>
+        <HeaderLeft>
+          <HeaderTitle>Dashboard</HeaderTitle>
+          <HeaderSubTitle>Visão geral dos dados</HeaderSubTitle>
+        </HeaderLeft>
+      </Header>
+    </div>
   );
 }
